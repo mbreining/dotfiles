0f634237f3745dc081a1dae7a916afa8199cd65a
Martin Breining <martin.breining@gmail.com>
2016-01-09 16:27:14 -0800

    Do not source control plugins; Update README and install.sh

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ dotfiles
 ========
 
 My dotfiles
+
+Installation
+------------
+
+$ git clone git@github.com:mbreining/dotfiles.git
+$ cd dotfiles
+$ ./install.sh
--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
-# install.sh: Install dotfiles on current system.
+# Install dotfiles under $HOME.
+#
+# Usage:
+# $ ./install.sh
+
+files_to_skip="README.md install.sh powerline"
+
+function skip_file() {
+  if [[ $files_to_skip =~ $1 ]]; then
+    return 0  # true
+  else
+    return 1  # false
+  fi
+}
 
 for name in *; do
   target="$HOME/.$name"
-  if [[ ! -e $target ]]; then
-    if [[ $name != 'install.sh' ]] && [[ $name != 'README.md' ]]; then
-      echo "Creating symlink for $target"
+  if [[ -e $target ]]; then
+    echo "$target already installed; skipping..."
+  else
+    if ! skip_file $name; then
+      #if [[ $name != 'install.sh' ]] && [[ $name != 'README.md' ]]; then
+      echo "$target not yet installed; creating symlink..."
       ln -s "$PWD/$name" "$target"
     fi
   fi
